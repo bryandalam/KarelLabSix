31e503211fa315b674cf8d12ac8c1fdedf7e47e8
Lam <2006220@student.acalanes.k12.ca.us>
2017-09-26 12:49:08 -0700

    Lab #6 Problem 1 Attempt

--- BeeperBot.java
+++ BeeperBot.java
@@ -16,13 +16,54 @@ public class BeeperBot extends Robot
     }
     
     public void findAndCountBeepers() {
-        
+        findBeepers();
+        countBeepers();
     }
     
     public int[] getBeeperList() {
             //This will be used to print the beepers found.  No need to modify.
             return beeperList;
+        }
+    public void findBeepers() {
+        faceEast();
+        while (!nextToABeeper()) {
+            move();
+        }
+        faceNorth();
     }
+    public void countBeepers() {
+        while (nextToABeeper()) {
+            pickBeeper();
+            tempCount++;
+            move();
+            if (!nextToABeeper()) {
+                returnToBottom();
+            }
+        }
+    }
+    public void returnToBottom() {
+        faceSouth();
+        while (frontIsClear()) {
+            move();
+        }
+    }
+    public void faceNorth() {
+        while (!facingNorth()) {
+            turnLeft();
+        }
+    }
+    public void faceSouth() {
+        while (!facingSouth()) {
+            turnLeft();
+        }
+    }
+    public void faceEast() {
+        while (!facingEast()) {
+            turnLeft();
+        }
+    }
+    
+    
    
 }
 
--- Problem1.java
+++ Problem1.java
@@ -10,10 +10,89 @@ public class Problem1 extends Robot
     public Problem1(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
-    
+    int[] beeperStack = new int[8];
+    int beepers;
+    int i;
+    int tempVar;
     public void sortBeepers() {
+        startingPosition();
+        calculateBeepers();
+        restackBeepers();
+    }
+    public void startingPosition() {
+        faceWest();
+        while (frontIsClear()) {
+            move();
+        }
+        faceSouth();
+        while (frontIsClear()) {
+            move();
+        }
+    }
+    public void calculateBeepers() {
+        faceNorth();
+        while (nextToABeeper()) {
+            pickBeeper();
+            beepers++;
+            move();
+        }
+        beeperStack[i] = beepers;
+        rePosition();
+    }
+    public void rePosition() {
+        faceSouth();
+        beepers = 0;
+        i++;
+        while(frontIsClear()) {
+            move();
+        }
+        if (!frontIsClear()) {
+            turnLeft();
+            move();
+            if (!nextToABeeper()) {
+                sortBeeper();
+            }
+            calculateBeepers();
+        }
+    }
+    public void faceNorth() {
+        while (!facingNorth()) {
+            turnLeft();
+        }
+    }
+    public void faceSouth() {
+        while (!facingSouth()) {
+            turnLeft(); 
+        }
+    }
+    public void sortBeeper() {
+        startingPosition();
+        faceNorth();
+        leastToGreatest();
+     }
+    public void leastToGreatest() {
+        for(int i=0; i < beeperStack.length; i++) {
+         System.out.print(beeperStack[i] + " ");
+        }
+       }
+    public void restackBeepers() {
+        for (int i = 0; i < beeperStack.length; i++) {
+            beeperStack[i + 1] = tempVar;
+            if (beeperStack[i] > tempVar) {
+                beeperStack[i] = tempVar;
+            }
+            if (beeperStack[i] < tempVar) {
+                tempVar = beeperStack[i + 2];
+            }
+        }
         
     }
+    public void faceWest() {
+        while (!facingWest()) {
+            turnLeft();
+        }
+    }
+         
    
 }
 
